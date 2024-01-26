5f40c05120f3e80a9a35be4f445fcda29d0a28c7
repotfm <wiliansyob@gmail.com>
2024-01-26 11:56:39 +0100

    Primer commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$headers = getallheaders();
+$headersText = '';
+foreach ($headers as $key => $value) {
+    $headersText .= htmlspecialchars($key) . ": " . htmlspecialchars($value) . "\n";
+}
+?>
+<?php
+$domain = $_SERVER['HTTP_HOST'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$serverIp = gethostbyname(gethostname());
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Información del Servidor</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div id="container">
+        <h1>Información del Servidor</h1>
+        <p>Dominio: <?php echo htmlspecialchars($domain); ?></p>
+        <p>Nombre del Contenedor: <strong><?php echo htmlspecialchars(gethostname()); ?></strong></p>
+        <p>IP del servidor: <?php echo htmlspecialchars($serverIp); ?></p>
+
+        <p>Servidor que responde: nginx</p>
+
+        <h2>Cabeceras HTTP:</h2>
+        <pre id="headers"><?php echo $headersText; ?></pre>
+    </div>
+
+</body>
+</html>
